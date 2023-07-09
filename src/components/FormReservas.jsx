c9cb9151a52ae5011967c7182f7470f7ed4e937a
edarvichm <edarvichm@gmail.com>
2023-07-08 21:50:31 -0400

    Ingreso Formualrio

--- src/components/FormReservas.jsx
+++ src/components/FormReservas.jsx
@@ -1,13 +1,44 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-const FormReservas = props => {
+const FormReservas = (props) => {
   return (
-    <div>Formulario de Reservas</div>
-  )
-}
+    <>
+      <div className="h5">Formulario de Reservas</div>
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Nombre</label>
+          <input type="text" className="form-control" id="InputName" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Correo Electrónico</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            Nunca compatiremos tu email con nadie.
+          </div>
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label">Check me out</label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Enviar
+        </button>
+      </form>
+    </>
+  );
+};
 
-FormReservas.propTypes = {}
+FormReservas.propTypes = {};
 
-export default FormReservas
+export default FormReservas;
--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable react/prop-types */
 // import React from "react";
 
 import FormReservas from "../components/FormReservas";
 // {menu}
-// const ReservasPage = ({menu} ) => {
-const ReservasPage = () => {
+
+const ReservasPage = ({ menu }) => {
+  // const ReservasPage = () => {
   return (
     <>
       <FormReservas />
-      {/* {menu} */}
+      {menu}
     </>
   );
 };
